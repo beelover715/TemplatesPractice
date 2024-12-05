c0ca1e87bdc985ea3f2e437ebea7150882b7c312
beelover715 <charles.schneider@sinclair.edu>
2024-12-04 20:20:38 -0500

    Add project files.

---
+++ TemplatesPractice/HalfProgram.cpp
@@ -0,0 +1,27 @@
+// Charles Schneider
+// CIS 1202 N01
+// December 4th, 2024
+
+#include <iostream>
+
+using namespace std;
+
+//template holder
+template <typename floatType>
+
+//function prototypes
+floatType half(floatType);
+int half(int);
+
+int main() {
+
+	return 0;
+}
+
+template <typename floatType>
+floatType half(floatType templateNum) {
+	
+}
+int half(int halfNum) {
+
+}
